f82985615966889787b5bbf200f995c09726940f
ana_j_bernardes <ana_j_bernardes@SNRSL1ELBIET045>
2016-11-24 10:34:37 -0200

    removido o arquivo ana.php

--- sobre.php
+++ sobre.php
@@ -1,11 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            .center {
+                margin: auto;
+                width: 60%;
+                padding: 10px;
+            }
+           .img {
+                margin: 5px;
+                border: 1px solid #000000;
+                float: left;
+                width: 190px;
+            }
+          .img img {
+    width: 190px                                        ;
+    height: auto;
+}
+        </style>
+    </head>
+    <body>
+        <div class="center">
+            <h2>História</h2>
 
-<h1>História</h1>
-<br>
-<p>Fundada em 18 de junho de 1998, por Camila Farias, Heric Bitencourt , Andrei Silva, Rafael Gomes.Com o objetivo de propocionar as novas gerações uma integração maior com o mundo tecnológico , criaram a ITSolutons para poder vender uma gama de produtos de ótima qualidade.Ao longo do tempo a loja adquiriu uma nova cara e um novo ramo no ano de 2005 além de venderem produtos inovaram dando suporte aos micros de seus clientes a empresa cresceu ganhou nova sede e expandiu o numero de funcionaros os quais antes haviam apenas 8 hoje contamos com mais de vinte funcionários bem qualificados em suas áreas.</p>
-<br>
-<p>Horário de atendimento: 09:00 às 19:00 de segunda à sexta-feira,
-horário de Brasília (exceto feriados).</p>
-<p>Endereço: Rua Santa Cruz, 787 - L1 </p>
-<p>Centro - Limeira (SP) - CEP: 13480-041 </p>
-<p>Central SAC: (19) 2114-4444 </p>
-<p>E-mail: atendimento@itsolutionecia.com.br </p>
+            <p>Fundada em 18 de junho de 1998, por Camila Farias, Heric Bitencourt , Andrei Silva, Rafael Gomes. Com o objetivo de propocionar as novas gerações, uma integração maior com o mundo tecnológico , criaram a ITSolutions para poder vender uma gama de produtos de ótima qualidade. Ao longo do tempo a loja adquiriu uma nova cara e um novo ramo. No ano de 2005 além de venderem produtos, inovaram, dando suporte aos micros de seus clientes. A empresa cresceu ganhou nova sede e expandiu o numero de funcionarios, no quais antes haviam apenas 8, e hoje contamos com mais de vinte funcionários bem qualificados em suas áreas.</p> 
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="img">
+                <a href="img/manutencao1.jpg">
+                    <img src="img/manutencao1.jpg"  height="700">
+                    </a>
+                 <div class="desc"><b>Laboratório de Manutenção</b></div>
+                  </div>
+                                      
+            <div class="img">
+                <a href="img/fachada.jpg">
+                    <img src="img/fachada.jpg"  height="700">
+                </a>
+                <div class="desc"><b>Fachada da Empresa</b></div>
+            </div>
+            <br>
+            <br>
+              <br>
+               <br>  <br>   <br>   <br>   <br>  <br>  <br>  <br>  <br>  <br>  <br>  <br>
+                <p>Horário de atendimento: 09:00 às 19:00 de segunda à sexta-feira,
+                    horário de Brasília (exceto feriados).</p>
+                <p>Endereço: Rua Santa Cruz, 787 - L1 </p>
+                <p>Centro - Limeira (SP) - CEP: 13480-041 </p>
+                <p>Central SAC: (19) 2114-4444 </p>
+                <p>E-mail: atendimento@itsolutionecia.com.br </p>
+        </div>
+
+    </body>
+</html>
