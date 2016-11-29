63cfd2bcb8d8821c7ed43375eb86ee9fecf47039
adrya <adrya@DESKTOP-QKA30EO>
2016-11-28 22:21:14 -0200

    Páginas adicionadas, páginas reconfiguradas, etc; TUDO FUNCIONANDO NORMALMENTE... #Acabado...

--- home.php
+++ home.php
@@ -17,23 +17,23 @@ $dadosProdutos = $DaoSite->listarHome();
 </div>
 <br>
 <div class="promocao">
-<?php
-foreach ($dadosProdutos as $rowProduto) {
-    ?>
-    <div class="img">
-        <a href="?pg=detalhes&codigo=<?=$rowProduto["codigo"]?>" style="text-decoration:none">
-            <img src="http://127.0.0.1/gestao_itsolution/fotos/<?=$rowProduto["imagem"]?>" alt="<?=$rowProduto["nome_completo"]?>" width="300" height="200">
-  
-        <div class="titulo_produto"><?=$rowProduto["nome_completo"]?></div>
-        <div class="desc"><?=$rowProduto["descricao"]?></div>
-        <div class="desc"><?= $rowProduto["categoria"] ?></div>
-        <div class="desc">R$ <?=$rowProduto["preco_custo"]?></div>
-         </a>
-        <center><a href="#" class="btn btn-primary btn-primary"><span class="glyphicon glyphicon-shopping-cart"></span> Comprar</a></center>
-    </div>
-
     <?php
-}
-?>
+    foreach ($dadosProdutos as $rowProduto) {
+        ?>
+        <div class="img">
+            <a href="?pg=detalhes&codigo=<?= $rowProduto["codigo"] ?>" style="text-decoration:none">
+                <img src="http://127.0.0.1/gestao_itsolution/fotos/<?= $rowProduto["imagem"] ?>" alt="<?= $rowProduto["nome_completo"] ?>" width="300" height="200">
+
+                <div class="titulo_produto"><?= $rowProduto["nome_completo"] ?></div>
+                <div class="desc" style="width: 250px; height: 240px; overflow: auto;"><?= $rowProduto["descricao"] ?></div>
+                <div class="desc"><?= $rowProduto["categoria"] ?></div>
+                <div class="desc">R$ <?= $rowProduto["preco_custo"] ?></div>
+            </a>
+            <center><a href="#" class="btn btn-primary btn-primary"><span class="glyphicon glyphicon-shopping-cart"></span> Comprar</a></center>
+        </div>
+
+        <?php
+    }
+    ?>
 </div>
 
