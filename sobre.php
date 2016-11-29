6224bd290ee2ca5da54b5c1e169253b978a04be3
adryan_silva <adryan_silva@SNRSL1ELBIET043>
2016-11-29 12:00:09 -0200

    commit by sou fodão

--- sobre.php
+++ sobre.php
@@ -7,27 +7,32 @@
                 width: 60%;
                 padding: 10px;
             }
-           .img {
+            .img {
                 margin: 5px;
                 border: 1px solid #000000;
                 float: left;
                 width: 190px;
             }
-          .img img {
-    width: 190px                                        ;
-    height: auto;
-}
+            .img img {
+                width: 190px                                        ;
+                height: auto;
+            }
         </style>
     </head>
     <body>
         <div class="center">
-           <h1>História</h1>
-<br>
-<p>Fundada em 18 de junho de 1998, por Camila Farias, Heric Bitencourt , Andrei Silva, Rafael Gomes.Com o objetivo de propocionar as novas gerações uma integração maior com o mundo tecnológico , criaram a ITSolutons para poder vender uma gama de produtos de ótima qualidade.Ao longo do tempo a loja adquiriu uma nova cara e um novo ramo no ano de 2005 além de venderem produtos inovaram dando suporte aos micros de seus clientes a empresa cresceu ganhou nova sede e expandiu o numero de funcionaros os quais antes haviam apenas 8 hoje contamos com mais de vinte funcionários bem qualificados em suas áreas.</p>
-<br>
-<p>Horário de atendimento: 09:00 às 19:00 de segunda à sexta-feira,
-horário de Brasília (exceto feriados).</p>
-<p>Endereço: Rua Santa Cruz, 787 - L1 </p>
-<p>Centro - Limeira (SP) - CEP: 13480-041 </p>
-<p>Central SAC: (19) 2114-4444 </p>
-<p>E-mail: atendimento@itsolutionecia.com.br </p>
+            <h1>História</h1>
+            <br>
+            <p>Fundada em 18 de junho de 1998, por Camila Farias, Heric Bitencourt , Andrei Silva, Rafael Gomes.Com o objetivo de propocionar as novas gerações uma integração maior com o mundo tecnológico , criaram a ITSolutons para poder vender uma gama de produtos de ótima qualidade.Ao longo do tempo a loja adquiriu uma nova cara e um novo ramo no ano de 2005 além de venderem produtos inovaram dando suporte aos micros de seus clientes a empresa cresceu ganhou nova sede e expandiu o numero de funcionaros os quais antes haviam apenas 8 hoje contamos com mais de vinte funcionários bem qualificados em suas áreas.</p>
+            <br>
+            <h2>Programa de Descarte</h2>
+            <p>Placas-Mães, Processadres, Memórias, e outros produtos tem que ter o seu descarte correto por que podem ser prejudicias para o meio ambiente por conta de seu aterial usado na sua produção. Nossa empresa tem a total maneira correta de descarte de produtos eletrônicos.</p>
+            <img src="img/1.png"alt="DESCARTE DE PRODUTOS IMG"/>
+            <br>
+            <br>
+            <p>Horário de atendimento: 09:00 às 19:00 de segunda à sexta-feira,
+                horário de Brasília (exceto feriados).</p>
+            <p>Endereço: Rua Santa Cruz, 787 - L1 </p>
+            <p>Centro - Limeira (SP) - CEP: 13480-041 </p>
+            <p>Central SAC: (19) 2114-4444 </p>
+            <p>E-mail: atendimento@itsolutionecia.com.br </p>
